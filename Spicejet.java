3fef9cc0ea65c7fb8d4000b1ca2f0c67c766342b
Eeshan <eeshanacharya@gmail.com>
2024-05-07 20:39:33 +0530

    Spicejet.java

--- Spicejet.java
+++ Spicejet.java
@@ -18,9 +18,7 @@ public class Spicejet {
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	WebElement login=driver.findElement(By.xpath("(//div[.='Login'])[1]"));
 	login.click();
-	driver.findElement(By.xpath("(//input[@autocapitalize='sentences'])[3]")).sendKeys("9861073595");
-	driver.findElement(By.xpath("(//input[@autocapitalize='sentences'])[4]")).sendKeys("Eeshan@1992");
-	driver.findElement(By.xpath("//div[@class='css-1dbjc4n r-1awozwy r-184aecr r-z2wwpe r-1loqt21 r-18u37iz r-tmtnm0 r-1777fci r-1x0uki6 r-1w50u8q r-ah5dr5 r-1otgn73']")).click();
+	click();
     
 	
 	}
